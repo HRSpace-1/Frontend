efcb194b59727c47facc547b1c94c726bb3ac65b
Elizaveta Ananeva-Ryashchenko <elizaveta.elenovna@yandex.ru>
2024-03-25 13:10:45 +0300

    feat: реализована работа кнопки info в карточке

--- src/components/form-info-card/form-info-card.tsx
+++ src/components/form-info-card/form-info-card.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, MouseEvent, useState } from 'react'
 import IconInfo from '../../images/icons/info.svg'
 import styles from './form-info-card.module.scss'
 
@@ -6,17 +6,33 @@ interface IFormInfoCard {
   title: string
   text: string
   info?: boolean
+  infoText?: string
 }
 
-const FormInfoCard: FC<IFormInfoCard> = ({ title, text, info = false }) => {
-  const active = false
+const FormInfoCard: FC<IFormInfoCard> = ({
+  title,
+  text,
+  info = false,
+  infoText = ''
+}) => {
+  const [isActive, setActive] = useState(false)
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    setActive(!isActive)
+  }
 
   return (
-    <div className={`${styles.infoCard} ${active && styles.active}`}>
+    <div className={`${styles.infoCard} ${isActive && styles.active}`}>
       <h4 className={styles.title}>{title}</h4>
-      <p className={styles.text}>{text}</p>
+      {isActive ? (
+        <p className={styles.text}>{infoText}</p>
+      ) : (
+        <p className={styles.text}>{text}</p>
+      )}
+
       {info && (
-        <button className={styles.info}>
+        <button className={styles.info} onClick={handleClick}>
           <IconInfo className={styles.icon} />
         </button>
       )}
